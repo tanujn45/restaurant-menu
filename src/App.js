857349e36ffcf233368a7dc9871cb958590552fa
tanujn45 <tanujn45@gmail.com>
2021-08-23 03:30:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from './img/logo.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section id="">
+        <div className="container">
+          <div className="d-flex flex-row align-items-center justify-content-between my-4">
+            <h4 className="mb-0 fw-bold">Demo Food</h4>
+            <img src={logo} alt="Logo" className="logo img-fluid" />
+          </div>
+          <div>
+            <h2 className="mb-0">Breakfast</h2>
+            <hr className="my-0 mb-4" />
+            <h5>Seasonal fresh fruit juice</h5>
+            <h6>Rs 100</h6>
+            <h5>Fresh fruit</h5>
+            <h6>Rs 100</h6>
+            <h5>Breakfast pastries / toast</h5>
+            <h6>Rs 100</h6>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
