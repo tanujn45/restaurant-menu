74d85f7848c50ee91d64b7900bd76ac9a51551dc
tanujn45 <tanujn45@gmail.com>
2021-09-08 14:05:26 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,115 @@
+import { useState } from 'react'
 import "./App.css";
 import logo from './img/logo.png'
+import veg from './img/veg.png'
+import nveg from './img/nveg.png'
 
 function App() {
+
+  const [foodMenu, setFoodMenu] = useState([
+    {
+      c_id: 1,
+      img: "https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Neha_Mathur/Achari_Paneer_Tikka_Recipe_Party_Food.JPG",
+      category: "soups",
+      items: [{
+        i_id: 1,
+        vn: 'veg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      }, {
+        i_id: 2,
+        vn: 'veg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      }, {
+        i_id: 3,
+        vn: 'veg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      }, {
+        i_id: 4,
+        vn: 'veg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      },
+      ]
+    }, {
+      c_id: 2,
+      img: "https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Neha_Mathur/Achari_Paneer_Tikka_Recipe_Party_Food.JPG",
+      category: "main course",
+      items: [{
+        i_id: 1,
+        vn: 'veg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      }, {
+        i_id: 2,
+        vn: 'veg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      }, {
+        i_id: 3,
+        vn: 'nveg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      }, {
+        i_id: 4,
+        vn: 'nveg',
+        name: 'Lemon Corriander Soup',
+        price: '100',
+      },
+      ]
+    }
+  ])
   return (
     <>
-      <section id="">
+      <section>
         <div className="container">
-          <div className="d-flex flex-row align-items-center justify-content-between my-4">
-            <h4 className="mb-0 fw-bold">Demo Food</h4>
-            <img src={logo} alt="Logo" className="logo img-fluid" />
+          <div className="text-center text-uppercase my-3">
+            <h3 className="text-danger font-bold">Pishori Dhaba</h3>
           </div>
-          <div>
-            <h2 className="mb-0">Breakfast</h2>
-            <hr className="my-0 mb-4" />
-            <h5>Seasonal fresh fruit juice</h5>
-            <h6>Rs 100</h6>
-            <h5>Fresh fruit</h5>
-            <h6>Rs 100</h6>
-            <h5>Breakfast pastries / toast</h5>
-            <h6>Rs 100</h6>
+
+          <img src="https://www.archanaskitchen.com/images/archanaskitchen/1-Author/Neha_Mathur/Achari_Paneer_Tikka_Recipe_Party_Food.JPG" alt="Paneer Tikka" className="img-fluid rounded mb-4" />
+
+          <div className="accordion">
+            {foodMenu.map(data => (
+              <div key={data.c_id} className="accordion-item">
+                <h2 className="accordion-header" id="panelsStayOpen-headingOne">
+                  <button className="accordion-button collapsed p-0 pe-3" type="button" data-bs-toggle="collapse" data-bs-target={"#panelsStayOpen-collapse" + data.c_id} aria-expanded="true" aria-controls={"panelsStayOpen-collapse" + data.c_id} >
+                    <div className="d-flex align-items-center justify-content-center">
+                      <img src={data.img} alt="Soups" className="img-fluid menu-img rounded me-3" />
+                      <h4 className="mb-0">{data.category}</h4>
+                    </div>
+                  </button>
+                </h2>
+                <div id={"panelsStayOpen-collapse" + data.c_id} className="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingOne">
+                  <div className="accordion-body">
+                    <div className="row align-items-center">
+                      {data.items.map(food => (
+                        <>
+                          <div className="col-10 mb-3 d-inline-flex align-items-center">
+                            {food.vn === "veg" ? <img src={veg} alt="veg" className="vnveg" /> : <img src={nveg} alt="nveg" className="vnveg" />}
+                            <h5 className="mb-0">{food.name}</h5>
+                          </div>
+                          <div className="col-2 mb-3">
+                            <h6>&#x20B9; {food.price}</h6>
+                          </div>
+                        </>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </section>
+      <footer id="footer">
+        <div className="d-flex align-items-center justify-content-center text-nowrap">
+          <p className="mb-0 me-1">Developed with &hearts; by</p>
+          <img src={logo} alt="Logo" className="w-logo img-fluid" />
+        </div>
+      </footer>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
-
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
